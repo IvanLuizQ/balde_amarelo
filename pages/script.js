6a9498c2210fbeedb797b4e91de66b7de643693d
IvanLuizQ <100075824+IvanLuizQ@users.noreply.github.com>
2023-02-07 12:01:11 -0300

    Novas Funções

---
+++ pages/script.js
@@ -0,0 +1,15 @@
+
+function menuCell(){
+
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    }else{
+        itens.style.display = 'block'
+    }
+
+}
+
+function inicio(){
+    inicio_t.style.display = 'none'
+    teste.style.display = 'block'
+}
